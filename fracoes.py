7e510f40a49c463ade517ef7af0960555a026a69
leonardorimolo <leonardorimolo1331@gmail.com>
2023-09-07 18:56:28 -0300

    Implementada as operações que restavam e testes

--- fracoes.py
+++ fracoes.py
@@ -8,10 +8,10 @@ class Fracao:
             mdc = Fracao.mdc(denominador, numerador)
             if denominador < 0:
                 mdc = -mdc
-            self._numerador = numerador // mdc
-            self._denominador = denominador // mdc
+            self._numerador = numerador // mdc #simplificando a fracao dividindo pelo seu mdc
+            self._denominador = denominador // mdc #simplificando a fracao dividindo pelo seu mdc
 
-
+    #CALCULO MDC
     def mdc(a: int, b: int) -> int:
         " Calcular o maximo divisor comum de a,b."
         while b != 0:
@@ -20,40 +20,102 @@ class Fracao:
             b = r
         return a
 
+
+    #----OPERAÇÕES MATEMÁTICAS----
+
+    #ADIÇÃO
     def __add__(self, other):
         n = self._numerador * other._denominador + self._denominador * other._numerador
         d = self._denominador * other._denominador
         return Fracao(n,d)
 
+    #SUBTRAÇÃO
     def __sub__(self, other):
         n = self._numerador * other._denominador - self._denominador * other._numerador
         d = self._denominador * other._denominador
         return Fracao(n,d)
+    
+    #MULTIPLICAÇÃO
+    def __mul__(self,other):
+        n = self._numerador * other._numerador
+        d = self._denominador * other._denominador
+        return Fracao(n,d)
 
-    #FALTA ADICIONAR A MULTIPLICACAO
-    #FALTA ADICIONAR A DIVISAO
+    #DIVISÃO
+    def __truediv__(self,other):
+        n = self._numerador * other._denominador
+        d = self._denominador * other._numerador
+        return Fracao(n,d)
+    
 
+
+
+
+    #----OPERADORES RELACIONAIS-----
+
+    #MENOR
     def __lt__(self, other):
-        return  self._numerador * other._numerador < self._denominador * other._numerador
+        return self._numerador * other._denominador < self._denominador * other._numerador
+         
+    #IGUAL
+    def __eq__(self,other):
+        if self._numerador == other._numerador and self._denominador == other._denominador:
+            return True
+        return False
+    
+    #MENOR IGUAL
+    def __le__(self):
+        return Fracao.__lt__ or Fracao.__eq__
 
+
+
+
+
+
+
+    #----CONVERSÕES DE TIPO----
+
+    #STRING
     def __str__(self):
        return f"{self._numerador} / {self._denominador}"
+    
+    #FLOAT
+    def __float__(self):
+        return float(self._numerador / self._denominador)
+    
+    #INT
+    def __int__(self):
+        return int(self._numerador / self._denominador)
 
 
 
 
 
 
-fracao = Fracao(16,10)
 
-print(fracao)
+
+#-----TESTES------
 
 f1 = Fracao ( 16 , 9 )
 f2 = Fracao (3 , 4 )
-
+f3 = Fracao (5 , 25 )
+f4 = Fracao ( 16 , 4 )
+f5 = Fracao (5 , 4 )
 print ( f1 )
 print ( f2 )
-f3 = Fracao (5 , 25 )
-
+print ( f3 )
+print ( f4 )
+print ( f5 )
 print ("F1 + F2 =", f1 + f2 )
 print ("F2 - F3 =", f2 - f3 )
+print ("F3 * F4 =", f3 * f4 )
+print ("F4 / F5 =", f4 / f5 )
+print ("16/2 + 16/2 =", Fracao ( 16 , 2 ) + Fracao ( 16 , 2 ) )
+print ("F2 == F3 =", f2 == f3 )
+print ("F3 > F3 =", f3 > f3 )
+print ("F4 == 4/1 =", f4 == Fracao (4 , 1 ) )
+print ( f1 , float ( f1 ) , int( f1 ) )
+print ( f2 , float ( f2 ) , int( f2 ) )
+print ( f3 , float ( f3 ) , int( f3 ) )
+print ( f4 , float ( f4 ) , int( f4 ) )
+print ( f5 , float ( f5 ) , int( f5 ) )
